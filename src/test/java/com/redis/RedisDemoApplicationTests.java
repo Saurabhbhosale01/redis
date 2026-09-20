52a99d920bc5219f031179a9e3fa1d05c84079d4
Saurabh Bhosale <saurabhbhosale426@gmail.com>
2026-09-20 11:34:32 +0530

    Merge main into redis-first-branch

---
+++ src/test/java/com/redis/RedisDemoApplicationTests.java
@@ -0,0 +1,24 @@
+package com.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+class RedisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    void sum(){
+        redisTemplate.opsForValue().set("email","sau@gmail.com");
+        Object email = redisTemplate.opsForValue().get("email");
+    }
+
+}
